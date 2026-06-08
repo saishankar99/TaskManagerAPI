ff6ba3fad35246c2d9f48afc17ba1cbf03c558aa
K <sai-shankar.kurukuri@hpe.com>
2026-06-08 07:31:43 +0530

    Day 2: Added path parameter task_id and query parameter status

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 
 app = FastAPI()
+
+fake_tasks=[
+    {"id":1, "title":"Buy Groceries", "status":"pending"},
+    {"id":2, "title":"Read fastapi docs", "status":"done"},
+    {"id":3, "title":"Build task manager API", "status":"pending"}
+]
+
+
 
 @app.get("/")
 def read_root():
@@ -12,7 +20,19 @@ def health_check():
     return {"status": "ok"}
 
 @app.get("/tasks")
-def get_tasks():
-    return {"tasks":[], "total":0}
+def get_tasks(status: str | None = None):
+    if status==None:
+        return {"tasks":fake_tasks, "total":len(fake_tasks)}
+
+    filtered = [task for task in fake_tasks if task["status"]==status]
+
+    return {"tasks":filtered , "total": len(filtered)}
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in fake_tasks:
+        if task["id"]==task_id:
+            return task
+    return HTTPException(status_code=404, detail=f"Task with id {task_id} not found")
 
 
