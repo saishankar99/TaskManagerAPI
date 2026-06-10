5d2061cc1a6b20b7d7a2febfd2cf48e3a32d9dfc
K <sai-shankar.kurukuri@hpe.com>
2026-06-10 07:35:06 +0530

    Day 3: Added TaskCreate pydantic BaseModel and post \tasks operation.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class TaskCreate(BaseModel):
+    title: str
+    description: str | None=None
+    status: str 
+
 fake_tasks=[
-    {"id":1, "title":"Buy Groceries", "status":"pending"},
-    {"id":2, "title":"Read fastapi docs", "status":"done"},
-    {"id":3, "title":"Build task manager API", "status":"pending"}
+    {"id":1, "title":"Buy Groceries", "status":"pending","description":None},
+    {"id":2, "title":"Read fastapi docs", "status":"done","description": None},
+    {"id":3, "title":"Build task manager API", "status":"pending", "description": None}
 ]
 
 
@@ -20,19 +26,29 @@ def health_check():
     return {"status": "ok"}
 
 @app.get("/tasks")
-def get_tasks(status: str | None = None):
+def get_tasks(status: str | None = None,limit: int=10):
     if status==None:
-        return {"tasks":fake_tasks, "total":len(fake_tasks)}
+        return {"tasks":fake_tasks[:min(len(fake_tasks),limit)], "total":min(len(fake_tasks),limit)}
 
     filtered = [task for task in fake_tasks if task["status"]==status]
 
-    return {"tasks":filtered , "total": len(filtered)}
+    return {"tasks":filtered[:min(len(filtered),limit)] , "total": min(len(filtered),limit)}
 
 @app.get("/tasks/{task_id}")
 def get_task(task_id: int):
     for task in fake_tasks:
         if task["id"]==task_id:
             return task
-    return HTTPException(status_code=404, detail=f"Task with id {task_id} not found")
+    raise HTTPException(status_code=404, detail=f"Task with id {task_id} not found")
 
+@app.post("/tasks",status_code=201)
+def post_task(task: TaskCreate):
+    new_task={
+        "id": len(fake_tasks)+1,
+        "title": task.title,
+        "status": task.status,
+        "description": task.description
+    }
+    fake_tasks.append(new_task)
+    return new_task
 
