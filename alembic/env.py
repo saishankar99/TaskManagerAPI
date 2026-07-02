45b4941eb83aacd206b138cf3c759c26ec4ecc7a
saishankar99 <saikalyan.kurikuri@gmail.com>
2026-07-02 05:40:28 +0530

    Day-14 move config to .env using BaseSettings from pydantic_settings, add .env.example

--- alembic/env.py
+++ alembic/env.py
@@ -13,6 +13,8 @@ import models
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+from config import settings
+config.set_main_option("sqlalchemy.url",settings.database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- database.py
+++ database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from config import settings
 """Database url generally follows this structure 
 databasedriver://username:password@hostname:port/<databasename>"""
-DATABASE_URL="sqlite:///./tasks.db"
+DATABASE_URL=settings.database_url
 
 """Used to create an engine.. a pool of connections to the specified database ur with some connect_arguments"""
 engine=create_engine(DATABASE_URL,connect_args={"check_same_thread": False})
